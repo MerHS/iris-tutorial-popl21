5f147875747db0fc23d05fd6bbbc9904460a2f7d
MerHS <starvessel@naver.com>
2021-02-02 03:48:33 +0900

    finish 4

--- exercises/ex_04_parallel_add.v
+++ exercises/ex_04_parallel_add.v
@@ -174,8 +174,8 @@ Section proof3.
     iDestruct (own_valid_3 with "H1 H2 Hγ●") as "H3".
     rewrite -frac_auth_frag_op.
     rewrite Qp_half_half.
-    iDestruct "H3" as %?%frac_auth_agree.
-    rewrite (_: n = p + q); last by [].
+    iDestruct "H3" as %->%frac_auth_agree_L.
+    iPureIntro.
     by rewrite Nat2Z.inj_add.
   Qed.
 
@@ -212,6 +212,8 @@ Section proof3.
       wp_seq.
       iInv "Hinv" as (n) ">[Hr Hγ●]" "Hclose".
       wp_load.
+      (* iCombine "Hγ1◯ Hγ2◯" as "Hγ◯". *)
+      (* iDestruct (own_valid_2 with "Hγ● Hγ◯") as %->%frac_auth_agree_L. *)
       iDestruct (ghost_var_frac_agree with "Hγ● Hγ1◯ Hγ2◯") as %Hn; first by [].
       rewrite (_: n = 4); last first.
       { inversion Hn. lia. }
@@ -220,23 +222,171 @@ Section proof3.
       iFrame.
   Qed.
 
-
-  Fixpoint parallel_faa_n n : expr :=
-    match n with
-    | O => ( FAA "r" #2 )
-    | S n =>  (parallel_faa_n n) ||| ( FAA "r" #2 )
-    end.
+  Definition parallel_faa_n : val :=
+    rec: "parallel_faa_n" "n" "r" :=
+      if: (BinOp LeOp "n" #0) then FAA "r" #2 else ("parallel_faa_n" (BinOp MinusOp "n" #1) "r") ||| ( FAA "r" #2 ).
 
   Definition parallel_add_n n : expr :=
     let: "r" := ref #0 in
-      (parallel_faa_n n)
+      parallel_faa_n #n "r"
       ;;
-        !"r".
+      !"r".
 
   Lemma parallel_add_spec_n (n: nat) :
-    {{{ True }}} parallel_add_n n {{{ RET #(2 * n); True }}}.
-  P    roof.
-
-  Admitted.
-  
+    {{{ True }}} parallel_add_n n {{{ RET #(2 * (n + 1)); True }}}.
+  Proof.
+    iIntros (Φ) "_ Post".
+    unfold parallel_add_n. wp_alloc r as "Hr". wp_let.
+    iMod (own_alloc (●F 0 ⋅ ◯F 0)) as (γ) "[Hγ● Hγ◯]".
+    { by apply auth_both_valid. }
+    iMod (inv_alloc nroot _ (parallel_add_inv_3 r γ) with "[Hr Hγ●]") as "#Hinv".
+    { iExists _. iFrame. }
+    assert (forall (n k p: nat) q, ●F n ⋅ ◯F{q} p ~~> ●F (n + k) ⋅ ◯F{q} (p + k)) as Hup.
+    {
+      intros n0 k p q.
+      apply frac_auth_update.
+      rewrite (comm plus). rewrite [p + _](comm plus).
+      apply (op_local_update_discrete n0 p k).
+      auto.
+    }
+    destruct n.
+    - wp_rec. wp_pures.
+      wp_bind (FAA _ _).
+      iInv "Hinv" as (n) ">[Hr Hg]" "Hclose".
+      wp_faa.
+      iMod (own_update_2 _ _ _ (●F (n+2) ⋅ ◯F 2) with "Hg Hγ◯") as "[Hg Hγ◯]".
+      {
+        apply frac_auth_update.
+        rewrite (comm plus).
+        apply (op_local_update_discrete n 0 2).
+        auto.
+      }
+      iMod ("Hclose" with "[Hg Hr]").
+      { iExists (n + 2).
+        iFrame.
+        iNext.
+        rewrite Nat2Z.inj_add.
+        iFrame.
+      }
+      iModIntro.
+      wp_seq.
+      iInv "Hinv" as (l) ">[Hr Hg]" "Hclose".
+      wp_load.
+      iDestruct (own_valid_2 with "Hg Hγ◯") as %->%frac_auth_agree_L.
+      iMod ("Hclose" with "[Hg Hr]").
+      { iExists 2. iFrame. }
+      by iApply "Post".
+    - wp_rec.
+      wp_let.
+      wp_op.
+      wp_if_false.
+      pose nq:= (pos_to_Qp (Pos.of_nat (S n))).
+      pose lq:= (nq / (nq + 1))%Qp.
+      pose rq:= (1 / (nq + 1))%Qp.
+      rewrite (_: 1%Qp = (lq + rq)%Qp); last first.
+      {
+        subst lq rq.
+        rewrite -Qp_div_add_distr Qp_div_diag.
+        auto.
+      }
+      rewrite (frac_auth_frag_op lq rq 0 0).
+      iDestruct (own_op _ _ _ with "Hγ◯") as "[Hlq Hrq]".
+      wp_smart_apply (wp_par (λ _, own γ (◯F{lq} (2 * (S n)))) (λ _, own γ (◯F{rq} 2))
+                      with "[Hlq] [Hrq]").
+      + subst nq lq rq.
+        wp_op.
+        rewrite (_ : (_ - _)%Z = Z.of_nat n); last by lia.
+        remember (pos_to_Qp (Pos.of_nat (S n))) as nq.
+        remember (nq / (nq + 1))%Qp as lq.
+        assert (forall k, 0 <= k <= n ->
+                     inv nroot (parallel_add_inv_3 r γ) -∗
+                         own γ (◯F{((pos_to_Qp (Pos.of_nat (S k))) / (nq + 1))%Qp} 0) -∗
+                         WP parallel_faa_n #k #r {{ _, own γ (◯F{((pos_to_Qp (Pos.of_nat (S k))) / (nq + 1))%Qp} (2 * (S k))) }}) as HX.
+        {
+          intros.
+          iIntros "#Hinv Hk".
+          iInduction k as [|k] "IH" ; wp_rec.
+          - wp_pures.
+            iInv "Hinv" as (l) ">[Hr Hg]" "Hclose".
+            wp_faa.
+            iMod (own_update_2 _ _ _ (●F (l + 2) ⋅ ◯F{_} 2) with "Hg Hk") as "[Hl Hk]".
+            { rewrite {2}(_: 2 = 0 + 2); last by lia. by apply Hup. }
+            iMod ("Hclose" with "[Hr Hl]"); last by [].
+            iExists _. iFrame.
+            by rewrite Nat2Z.inj_add.
+          - wp_let.
+            wp_op.
+            wp_if.
+            remember (pos_to_Qp (Pos.of_nat (S k))) as kq'.
+            remember (kq' / (nq + 1))%Qp as lkq'.
+            remember (1 / (nq + 1))%Qp as rkq'.
+            remember (pos_to_Qp (Pos.of_nat (S (S k)))) as kq.
+            remember (kq / (nq + 1))%Qp as lkq.
+            rewrite (_: lkq = (lkq' + rkq')%Qp); last first.
+            {
+              subst.
+              rewrite -Qp_div_add_distr.
+              rewrite (_: pos_to_Qp (Pos.of_nat (S (S k))) = (pos_to_Qp (Pos.of_nat (S k)) + 1)%Qp); first by [].
+              rewrite pos_to_Qp_add.
+              apply pos_to_Qp_inj_iff.
+              lia.
+            }
+            rewrite (frac_auth_frag_op lkq' rkq' 0 0).
+            iDestruct (own_op _ _ _ with "Hk") as "[Hlq Hrq]".
+            wp_smart_apply (wp_par (λ _, own γ (◯F{lkq'} (2 * (S k)))) (λ _, own γ (◯F{rkq'} 2))
+                              with "[Hlq] [Hrq]").
+            + wp_op. rewrite (_: (S k - 1)%Z = Z.of_nat k); last by lia.
+              iApply "IH"; last by [].
+              iPureIntro. lia.
+            + iInv "Hinv" as (l) ">[Hr Hg]" "Hclose".
+              wp_faa.
+              iMod (own_update_2 _ _ _ (●F (l + 2) ⋅ ◯F{_} 2) with "Hg Hrq") as "[Hl Hrq]".
+              { rewrite {2}(_: 2 = 0 + 2); last by lia. by apply Hup. }
+              iMod ("Hclose" with "[Hr Hl]"); last by [].
+              iExists _. iFrame.
+              by rewrite Nat2Z.inj_add.
+            + iIntros (v1 v2) "[Hl Hr]".
+              iCombine "Hl Hr" as "H".
+              iNext.
+              rewrite (_: (2 * (S (S k))) = (2 * S k + 2)); [auto | lia].
+        }
+        destruct n.
+        { simpl.
+          wp_rec. wp_pures.
+          iInv "Hinv" as (l) ">[Hr Hg]" "Hclose".
+          wp_faa.
+          iMod (own_update_2 _ _ _ (●F (l + 2) ⋅ ◯F{lq} 2) with "Hg Hlq") as "[Hl Hrq]".
+          { rewrite {2}(_: 2 = 0 + 2); last by lia. by apply Hup. }
+          iMod ("Hclose" with "[Hr Hl]"); last by [].
+          iExists _. iFrame.
+          by rewrite Nat2Z.inj_add.
+        }
+        assert (0 <= (S n) /\ (S n) <= (S n)) as Hn; first by lia.
+        subst.
+        iApply ((HX (S n) Hn) with "Hinv Hlq").
+      + iInv "Hinv" as (l) ">[Hr Hg]" "Hclose".
+        wp_faa.
+        iMod (own_update_2 _ _ _ (●F (l + 2) ⋅ ◯F{rq} 2) with "Hg Hrq") as "[Hl Hrq]".
+        { rewrite {2}(_: 2 = 0 + 2); last by lia. by apply Hup. }
+        iMod ("Hclose" with "[Hr Hl]"); last by [].
+        iExists _. iFrame.
+        by rewrite Nat2Z.inj_add.
+      + iIntros (v1 v2) "[Hlq Hrq]".
+        iNext. wp_seq.
+        iInv "Hinv" as (l) ">[Hr Hg]" "Hclose".
+        wp_load.
+        iCombine "Hlq Hrq" as "Hl".
+        rewrite (_: (lq + rq)%Qp = 1%Qp); last first.
+        {
+          subst lq rq.
+          rewrite -Qp_div_add_distr Qp_div_diag.
+          auto.
+        }
+        iDestruct (own_valid_2 with "Hg Hl") as %->%frac_auth_agree_L.
+        iMod ("Hclose" with "[Hg Hr]").
+        { iExists _. iFrame. }
+        rewrite (_: 2 * S n + 2 = 2 * (S n + 1)); last by lia.
+        rewrite (_: Z.of_nat (2 * _)%nat = (2 * (S n + 1))%Z); last by lia.
+        by iApply "Post".
+    Qed.
 End proof3.
